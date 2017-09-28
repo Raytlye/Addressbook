65ca17e914fa60897439d4c15755d28dfbf47438
von Niederhäusern Lucas <lvonnied@hsr.ch>
2017-09-28 15:13:00 +0200

    Added logging

--- src/model/AddressBook.java
+++ src/model/AddressBook.java
@@ -2,6 +2,9 @@ package model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import logger.FileLogger;
 
 public class AddressBook {
 	
@@ -11,25 +14,33 @@ public class AddressBook {
 	private ArrayList<AddressBookListener> listeners 
 		= new ArrayList<AddressBookListener>();
 	
+	FileLogger fl = new FileLogger();
+	Logger logger = fl.getLogger();
+	
 	public void setAddressBook(File file) {
 		
+		logger.info("Reading from " + file + " to ArrayList<User> addressBook");
 		addressBook = reader.read(file);
 		
 	}
 	
 	public void setEmptyAddressBook() {
 		
+		logger.info("Setting addressBook as a new ArrayList<User>");
 		addressBook = new ArrayList<User>();
 		
 	}
 	
 	public void saveUser(File file) {
 		
+		logger.info("Saving addressbook to " + file);
 		writer.write(addressBook, file);
 		
 	}
 	
 	public void addUser(User user) {
+		
+		logger.info("Adding " + user.getNachname() + " to addressBook and iterating through the listeners");
 		
 		addressBook.add(user);
 		
@@ -43,6 +54,8 @@ public class AddressBook {
 	
 	public void removeUser(User user) {
 		
+		logger.info("Removing " + user.getNachname() + " from addressBook and iterating through the listeners");
+		
 		addressBook.remove(user);
 		for(AddressBookListener listener : listeners) {
 			
@@ -54,6 +67,7 @@ public class AddressBook {
 	
 	public void updateUser(User updatedUser, int index) {
 		
+		logger.info("Update " + updatedUser.getNachname() + " at Index " + index + " and iterating through the listeners");
 		User user = addressBook.get(index);
 		
 		user.setStudiengang(updatedUser.getStudiengang());
@@ -72,11 +86,15 @@ public class AddressBook {
 	
 	public ArrayList<User> getAddressBook() {
 		
+		logger.info("Returning addressBook");
+		
 		return addressBook;
 		
 	}
 	
 	public void addListener(AddressBookListener listener) {
+		
+		logger.info("Adding " + listener.getClass() + " to ArrayList listeners");
 		
 		listeners.add(listener);
 		
--- src/model/Controller.java
+++ src/model/Controller.java
@@ -3,12 +3,14 @@ package model;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.logging.Logger;
 
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+import logger.FileLogger;
 import view.AddressTable;
 import view.DeleteMessage;
 import view.ValueEditWindow;
@@ -24,6 +26,9 @@ public class Controller implements ActionListener{
 	private AddressTable adTable;
 	private boolean isSaved = true;
 	
+	FileLogger fl = new FileLogger();
+	Logger logger = fl.getLogger();
+	
 	public Controller(AddressBook book, AddressTable adTable) {
 		
 		this.book = book;
@@ -32,26 +37,31 @@ public class Controller implements ActionListener{
 	}
 
 	public boolean openFileChooser() {
-		JFileChooser jfc = new JFileChooser("C:\\Users\\lvonnied\\eclipse-workspace\\Swing Address book");
+		logger.info("Opening JFileChooser at " + SAVE_LOCATION);
+		JFileChooser jfc = new JFileChooser(SAVE_LOCATION);
 
 		int returnValue = jfc.showOpenDialog(adTable);
 
 		if (returnValue == JFileChooser.APPROVE_OPTION) {
 			selectedFile = jfc.getSelectedFile();
 			book.setAddressBook(selectedFile);
+			logger.info("Opening " + selectedFile.getName() + " and setting AddressBook to it");
 			return true;
 		}else {
+			logger.info("User pressed Cancel");
 			return false;
 		}
 		
 	}
 	
 	public void openSaveAsDirectory() {
-		
+		logger.info("Opening JOptionPane to type name for File");
 		String name;
 		do {
+			logger.info("Repeating method until user types a correct name or cancels input");
 		    name = JOptionPane.showInputDialog(null, "Please type a name for your file");
 		    if(name == null) {
+		    	logger.info("User pressed Cancel");
 		        break;
 		    }
 		} while(name.isEmpty());
@@ -60,6 +70,7 @@ public class Controller implements ActionListener{
 			File fileName = new File(SAVE_LOCATION + FILE_SEPERATOR + name + FILE_SUFFIX);
 			book.saveUser(fileName);
 			isSaved = true;
+			logger.info("Saving file at " + fileName);
 		}
 		
 	}
@@ -67,9 +78,11 @@ public class Controller implements ActionListener{
 	public void openSaveDirectory() {
 		
 		if(selectedFile != null) {
+			logger.info("Saving into existing file...");
 			book.saveUser(selectedFile);
 			isSaved = true;
 		}else {
+			logger.info("Opening openSaveAsDirectory because a exisiting file to save into doesn't exist");
 			openSaveAsDirectory();
 		}
 		
@@ -77,12 +90,14 @@ public class Controller implements ActionListener{
 	
 	public void setEmptySelectedFile() {
 		
+		logger.info("Setting selectedFile to null");
 		selectedFile = null;
 		
 	}
 	
 	public void createJOptionSelectPane() {
 		
+		logger.info("Opening Warning Message (Didn't select a row)");
 		JDialog dialog = new JDialog();
 		JOptionPane.showMessageDialog(dialog,
 				"Please select one row!",
@@ -93,6 +108,7 @@ public class Controller implements ActionListener{
 	
 	public void createJOptionOpenPane() {
 		
+		logger.info("Opening Warning Message (User didn't open a file)");
 		JDialog dialog = new JDialog();
 		JOptionPane.showMessageDialog(dialog,
 				"Please open a file first!",
@@ -103,6 +119,7 @@ public class Controller implements ActionListener{
 	
 	public void checkCounter() {
 		
+		logger.info("Checking attribute createTableCounter");
 		if(adTable.createTableCounter > 0) {
 			adTable.removeTablePanel();
 		}
@@ -111,6 +128,7 @@ public class Controller implements ActionListener{
 	
 	public void createAndUpdateUI() {
 		
+		logger.info("Creating Panel, incrementing createTableCounter and updating GUI");
 		isSaved = false;
 		adTable.createTablePanel();
 		adTable.createTableCounter++;
@@ -126,6 +144,7 @@ public class Controller implements ActionListener{
 		switch(action) {
 		
 		case "NEW":
+			logger.info("Opening " + ValueInputWindow.class);
 			new ValueInputWindow(adTable, book);
 			
 			break;
@@ -134,7 +153,7 @@ public class Controller implements ActionListener{
 				
 			if(adTable.table.getSelectedRowCount() != 1) {createJOptionSelectPane();}
 			
-			else {new DeleteMessage(adTable, book, adTable.table);}
+			else {new DeleteMessage(adTable, book, adTable.table);logger.info("Opening " + DeleteMessage.class);}
 			
 			break;
 			
@@ -142,7 +161,7 @@ public class Controller implements ActionListener{
 			
 			if(adTable.table.getSelectedRowCount() != 1) {createJOptionSelectPane();}
 			
-			else {new ValueEditWindow(adTable, book, adTable.table);}
+			else {new ValueEditWindow(adTable, book, adTable.table);logger.info("Opening " + ValueEditWindow.class);}
 			
 			break;
 			
@@ -150,6 +169,7 @@ public class Controller implements ActionListener{
 			
 			if(openFileChooser() == true) {
 				if(!isSaved) {
+					logger.info("Opening Save Dialog because file wasn't saved yet");
 					int result = JOptionPane.showConfirmDialog(null, "Any unsaved changes will be lost. Do you wish to save first?",null, JOptionPane.YES_NO_OPTION);
 					if(result == JOptionPane.YES_OPTION) {
 						openSaveDirectory();
@@ -163,6 +183,7 @@ public class Controller implements ActionListener{
 			
 		case "NEWFILE":
 			if(!isSaved) {
+				logger.info("Opening Save Dialog because file wasn't saved yet");
 				int result = JOptionPane.showConfirmDialog(null, "Any unsaved changes will be lost. Do you wish to save first?",null, JOptionPane.YES_NO_OPTION);
 				if(result == JOptionPane.YES_OPTION) {
 					openSaveDirectory();
@@ -192,6 +213,7 @@ public class Controller implements ActionListener{
 		case "EXIT":
 			
 			if(!isSaved) {
+				logger.info("Opening Save Dialog because file wasn't saved yet");
 				int result = JOptionPane.showConfirmDialog(null, "Any unsaved changes will be lost. Do you wish to save first?",null, JOptionPane.YES_NO_OPTION);
 				if(result == JOptionPane.YES_OPTION) {
 					openSaveDirectory();
@@ -202,6 +224,7 @@ public class Controller implements ActionListener{
 			break;
 			
 		default :
+			logger.severe("Something went terribly wrong!");
 			JDialog dialog = new JDialog();
 			JOptionPane.showMessageDialog(dialog,
 				    "Something went horribly wrong!",
